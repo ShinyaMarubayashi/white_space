3535771b316136fcae4d48466e79674f07c41ec6
ShinyaMarubayashi <48169047+ShinyaMarubayashi@users.noreply.github.com>
2021-09-08 22:38:24 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.addEventListener('load', function () {
   // テキストスプライトを格納する配列
   let textSpriteArr = [];
   // Canvasのサイズ
-  const WIDTH = 00;
+  const WIDTH = 800;
   const HEIGHT = 500;
   // Pixiアプリケーションの生成
   let pixiApp = new PIXI.Application({ 
